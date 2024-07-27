27756540e3ba4d8b66c7d0cfe78cca7fa0214261
VolodymyrSVM <sviahlavolodymyr@gmail.com>
2024-07-27 15:33:55 +0300

    first file
    Я зробив щоь нове

---
+++ first_VS.py
@@ -0,0 +1,9 @@
+num_1 = int(input("1 число: "))
+num_2 = int(input("2 число: "))
+
+if num_1 > num_2:
+    print("Перше число більше")
+elif num_2 > num_1:
+    print("Друге число більше")
+else:
+    print("Чистла рівні")
